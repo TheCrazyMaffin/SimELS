cdd05ef815890892370f0eaa0d0fcd95603bcef8
TheCrazyMaffin <marvin@gutstein.xyz>
2024-10-02 09:03:05 +0200

    Initial commit

---
+++ settings.js
@@ -0,0 +1,5 @@
+// Map starting position [lng, lat]
+const mapCenter = {lng: 9, lat: 49};
+// Set dispatch area. Limits geocoding search results and map view to this area
+// Use https://bboxfinder.com [lng, lat, lng, lat]
+const bbox = [{lng: 8.95, lat: 48.95}, {lng: 9.10, lat: 49.03}];
